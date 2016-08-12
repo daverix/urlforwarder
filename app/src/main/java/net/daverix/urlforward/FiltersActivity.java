9e267ba724f3595b42b91301be59e3a9830ec4c6
David Laurell <david.laurell@gmail.com>
2016-08-12 12:11:32 +0200

    Remove need to create fragment in activity

--- app/src/main/java/net/daverix/urlforward/FiltersActivity.java
+++ app/src/main/java/net/daverix/urlforward/FiltersActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -25,14 +24,6 @@ public class FiltersActivity extends AppCompatActivity implements FiltersFragmen
                 startActivity(new Intent(Intent.ACTION_INSERT, UrlFilters.CONTENT_URI));
             }
         });
-
-        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragmentFilterList);
-        if(fragment == null) {
-            fragment = new FiltersFragment();
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.fragmentFilterList, fragment)
-                    .commit();
-        }
     }
 
     @Override
