9dccdf4b8801dc089f3bf88634b7e4a74dddd956
David Laurell <david.laurell@gmail.com>
2016-08-13 14:57:12 +0200

    Add GPL-3 license to every source file

--- app/src/androidTest/java/net/daverix/urlforward/Actions.java
+++ app/src/androidTest/java/net/daverix/urlforward/Actions.java
@@ -1,5 +1,21 @@
-package net.daverix.urlforward;
+/*
+    UrlForwarder makes it possible to use bookmarklets on Android
+    Copyright (C) 2016 David Laurell
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.daverix.urlforward;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.clearText;
